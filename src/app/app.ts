926ee0acc8906d91240b5621a6b8cf177e00bf8f
Uros <uroos.krstic29@gmail.com>
2026-03-11 14:53:58 +0100

    Add angular material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkWithHref],
+  imports: [RouterOutlet, RouterLinkWithHref,MatButtonModule,MatMenuModule,MatToolbarModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('klijentske-veb-app-2026');
-  ime = 'Uros'
-  prezime = 'Krstic'
-  indeks = '2023201180'
+  
 }
